72eb6aacddbfa77048bb10814d8f542d80911421
simper -frada <madefrada@gmail.com>
2021-11-16 23:37:21 +0700

    init nuxt

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+      
+    <div class="row">
+      <div class="one-half column" style="margin-top: 25%">
+        <h4>Portfolio</h4>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis tempora tenetur molestias et sapiente! Odit aliquid sit id sapiente blanditiis expedita quibusdam! Quisquam velit ab repellat dolor maiores tempora ratione!.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
